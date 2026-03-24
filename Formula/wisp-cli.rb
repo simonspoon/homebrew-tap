b477ab46baf6ab178823853d234e19b3f05eba29
Simon <simon@sjspoon.com>
2026-03-23 20:38:43 -0400

    update wisp-cli formula to v0.3.0

--- Formula/wisp-cli.rb
+++ Formula/wisp-cli.rb
@@ -1,26 +1,26 @@
 class WispCli < Formula
   desc "CLI companion for Wisp, a desktop UI design tool for agents"
   homepage "https://github.com/simonspoon/wisp"
-  version "0.1.0"
+  version "0.3.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/simonspoon/wisp/releases/download/v0.1.0/wisp-cli-darwin-arm64"
-      sha256 "d5b78be40ed1a0262967a28e8b154296f1560eb439db90f0c0a7045065c9751e"
+      url "https://github.com/simonspoon/wisp/releases/download/v0.3.0/wisp-cli-darwin-arm64"
+      sha256 "8e0ccc23513cb6878fd86a451bb188d67bbf2c4499803867dea0b33edf695ef8"
     else
-      url "https://github.com/simonspoon/wisp/releases/download/v0.1.0/wisp-cli-darwin-amd64"
-      sha256 "a3537850348e68a95f86ee0341ea9dae40da0f07776e5e26cafc3bd2192888bc"
+      url "https://github.com/simonspoon/wisp/releases/download/v0.3.0/wisp-cli-darwin-amd64"
+      sha256 "cc68527bf2732066110b7b17cd74621e60e4c9cdd819676f0b70da65a4590e22"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/simonspoon/wisp/releases/download/v0.1.0/wisp-cli-linux-arm64"
-      sha256 "8450fa15d6174fe0e786e4540e13df3f5200f4ce045fbb502da98f0155860aaa"
+      url "https://github.com/simonspoon/wisp/releases/download/v0.3.0/wisp-cli-linux-arm64"
+      sha256 "bbd6f3f97a3c32e9c15db93cff3041a6aae9a647e1397bdc33343804fb316fff"
     else
-      url "https://github.com/simonspoon/wisp/releases/download/v0.1.0/wisp-cli-linux-amd64"
-      sha256 "939862dccd9215425b3821497cc41287ff9c19acd150f6960187d584f937af1a"
+      url "https://github.com/simonspoon/wisp/releases/download/v0.3.0/wisp-cli-linux-amd64"
+      sha256 "68cec3b3321664839e13fa3d9a371afe22556928d63ae4b3bbe90026dc115d51"
     end
   end
 
