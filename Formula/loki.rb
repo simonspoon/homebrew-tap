031db2a9cace1be4c9fd88f04e8cce47aa091614
Simon <simon@sjspoon.com>
2026-03-25 10:19:32 -0400

    fix loki formula with actual release checksums

--- Formula/loki.rb
+++ Formula/loki.rb
@@ -9,10 +9,10 @@ class Loki < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/simonspoon/loki/releases/download/v0.1.0/loki-darwin-arm64"
-      sha256 "PLACEHOLDER_ARM64_SHA256"
+      sha256 "7f72a6e1e5c197b0a26fbe18d46cb51264b8081bc8b468e6cdc6681a34b1b70d"
     else
       url "https://github.com/simonspoon/loki/releases/download/v0.1.0/loki-darwin-amd64"
-      sha256 "PLACEHOLDER_AMD64_SHA256"
+      sha256 "f41f93ea10dad44b5f0dc2eeb742318127dd38d6fa89ea75c798383a09c9ba21"
     end
   end
 
